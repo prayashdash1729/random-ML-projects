e26c3cd418ff6995e843280360f31af6cb8a6181
Prayash Dash <prayashdash2003@gmail.com>
2023-06-04 17:35:35 +0530

    final commit

--- flask-tutorial/main.py
+++ flask-tutorial/main.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
 
+with open("/templates/config.json", "r") as c:
+    params = json.load(c)["params"]
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/prayash_blog"
+
+
+if params["local_server"]:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params["local_uri"]
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params["prod_uri"]
+
 db = SQLAlchemy(app)
 
 
@@ -14,22 +24,22 @@ class Contacts(db.Model):
     message = db.Column(db.String, nullable=False)
     phone_num = db.Column(db.String(12), nullable=False)
     email = db.Column(db.String, nullable=False)
-    date = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=True)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", params=params)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", params=params)
 
 
 @app.route("/post")
 def post():
-    return render_template("post.html")
+    return render_template("post.html", params=params)
 
 
 @app.route("/contact", methods=["GET", "POST"])
@@ -37,14 +47,14 @@ def contact():
     # Add entry to the database
     if request.method == "POST":
         name = request.form.get("name")
-        email = request.forms.get("email")
+        email = request.form.get("email")
         phone_num = request.form.get("phone_num")
         message = request.form.get("message")
 
-        entry = Contacts(name=name, phone_num=phone_num, message=message, email=email)
+        entry = Contacts(name=name, phone_num=phone_num, message=message, email=email, date=datetime.now())
         db.session.add(entry)
         db.session.commit()
-    return render_template("contact.html")
+    return render_template("contact.html", params=params)
 
 
 if __name__ == "__main__":
